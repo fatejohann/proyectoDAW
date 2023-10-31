0bb7a495629b3851ae44b5be18e25a86a67231da
fatejohann <holaquepasa70@gmail.com>
2023-10-31 17:25:52 -0400

    implementacion de firebase para registrar libros

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyD4ACtEfzRhkfHN2chIvtgece5KGscW78k",
+  authDomain: "proyectodaw-b3cbb.firebaseapp.com",
+  projectId: "proyectodaw-b3cbb",
+  storageBucket: "proyectodaw-b3cbb.appspot.com",
+  messagingSenderId: "442942467897",
+  appId: "1:442942467897:web:99ef7b75bbc4194dff02e8",
+  measurementId: "G-T3KSVM4TMR"
+    },
+  };
